8226fd2ec7514a7d90aec0c7a0f4ccf09850705d
Lilkhachatryan <Lilkhachatyan>
2020-01-26 04:57:55 -0800

    Updates

---
+++ precache-manifest.2e1baba7c0d012a99f1979ae74a35be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce3f4a8042824982fe2b8c34ae09f84",
+    "url": "/BloggingApp/index.html"
+  },
+  {
+    "revision": "5ad25a77432981c7e755",
+    "url": "/BloggingApp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ecfb4a537aeb1a0ed88e",
+    "url": "/BloggingApp/static/css/main.65cfe266.chunk.css"
+  },
+  {
+    "revision": "5ad25a77432981c7e755",
+    "url": "/BloggingApp/static/js/2.37832666.chunk.js"
+  },
+  {
+    "revision": "ecfb4a537aeb1a0ed88e",
+    "url": "/BloggingApp/static/js/main.0dbb8bea.chunk.js"
+  },
+  {
+    "revision": "69f355ad663ac11821d3",
+    "url": "/BloggingApp/static/js/runtime-main.c709e9d2.js"
+  }
+]);
