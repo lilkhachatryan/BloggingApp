4d930dabbfc37c0580bb2090885c293deaf707e7
Lilkhachatryan <Lilkhachatyan>
2020-03-16 01:30:07 +0400

    Reading List -> Saved, Archived
    and fix archive icon issue

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -35,7 +35,7 @@ function Posts(props) {
 
     const bookmarkPost = async (post) => {
         try {
-            await addBookmark({post: postRef(post.id), user: userRef(user.id)});
+            await addBookmark({ post: postRef(post.id), user: userRef(user.id), isArchived: false });
             handlePostsChange({isBookmarked: true, id: post.id});
         } catch (e) {
             console.log('bookmarkPost e', e);
@@ -78,7 +78,9 @@ function Posts(props) {
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     if (error) return (<div>Something went wrong.</div>);
+
     if (loading) return (<Loading />);
+
     if (!state.posts[0]) return (
         <Card  className="mx-auto mt-4 mb-4" >
             <Card.Body>
